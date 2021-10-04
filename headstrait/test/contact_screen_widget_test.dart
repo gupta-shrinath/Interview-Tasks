06f4f72920cc9061092082c62123c82d28747009
Gupta Shrinath <guptashrinath9@gmail.com>
2021-10-04 14:25:54 +0530

    Create widget test for future not resolved

--- headstrait/test/contact_screen_widget_test.dart
+++ headstrait/test/contact_screen_widget_test.dart
@@ -1,9 +1,26 @@
+import 'dart:async';
+
 import 'package:contacts/main.dart';
 import 'package:contacts/models/contact.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  testWidgets("Show circular progress indicator when future isn't complete",
+      (WidgetTester tester) async {
+    tester.runAsync(() async {
+      final contactsScreen = MaterialApp(
+        home: Scaffold(
+          body: buildContactList(
+            contacts: Future.delayed(Duration(milliseconds: 5)),
+          ),
+        ),
+      );
+      await tester.pumpWidget(contactsScreen);
+      expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    });
+  });
+
   testWidgets("Show error text widget when future returns error",
       (WidgetTester tester) async {
     final contactsScreen = MaterialApp(
